a1f223c824d3fd29f219e941deadbe9f4f3ab786
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-24 17:54:51 +0100

    added bonus folder

---
+++ array_bonus/js/script.js
@@ -0,0 +1,12 @@
+// Crazione del primo array
+let array_1 = ['1', '2', '3', '4', '5'];
+console.log(array_1);
+
+// Creazione del secondo array
+let array_2 = ['1', '2'];
+console.log(array_2);
+
+// Creazione del ciclo for
+for(i=0; i<=array_1.length; i++){
+    
+}

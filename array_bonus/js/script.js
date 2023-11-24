0d64c568a05a33c8d67a5de0d6916db667c8eabe
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-24 18:57:24 +0100

    added bonus in bonus folder

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,7 +1,25 @@
 // Crazione del primo array
-let array_1 = ['1', '2', '3', '4', '5'];
-console.log(array_1);
+let array1 = ['1', '2', '3', '4', '5'];
+console.log(array1);
 
 // Creazione del secondo array
-let array_2 = ['1', '2'];
-console.log(array_2);
+let array2 = ['1', '2'];
+console.log(array2);
+
+
+// Verifica quale array ha meno elementi
+let lengthDifference = Math.abs(array1.length - array2.length);
+
+// Aggiungi elementi all'array che ha meno elementi
+if (array1.length < array2.length) {
+  for (var i = 0; i < lengthDifference; i++) {
+    array1.push("" + (i + 1));
+  }
+} else if (array2.length < array1.length) {
+  for (var i = 0; i < lengthDifference; i++) {
+    array2.push("" + (i + 1));
+  }
+}
+
+console.log(array1);
+console.log(array2);
